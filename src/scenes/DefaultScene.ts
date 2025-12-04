f416cdb3c8f95ce0271a1ff9dec2a38219880f0d
samueljarry <samueljarry.sj@gmail.com>
2025-12-04 21:33:03 +0100

    update

--- src/scenes/DefaultScene.ts
+++ src/scenes/DefaultScene.ts
@@ -1,9 +1,9 @@
 import { AssetsId } from "@/core/commons/constants/AssetsId";
-import { CamerasId } from "@/core/three/constants/CamerasId";
 import { SceneId } from "@/core/commons/constants/scenes/SceneId";
 import { SceneLayer } from "@/core/commons/constants/scenes/SceneLayer";
-import { ThreeScene } from "@/core/three/scenes/ThreeScene";
 import { ViewId } from "@/core/commons/constants/views/ViewId";
+import { CamerasId } from "@/core/three/constants/CamerasId";
+import { ThreeScene } from "@/core/three/scenes/ThreeScene";
 
 export class DemoScene extends ThreeScene {
   constructor() {
@@ -13,6 +13,5 @@ export class DemoScene extends ThreeScene {
     })
 
     this._views.add(ViewId.THREE_DEMO);
-    this._views.add(ViewId.DEMO)
   }
 }
